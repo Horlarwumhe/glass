b15a4e88d876cf980c5cb78c1634297e9da0aaa4
Horlarwumhe <amachiever4real@gmail.com>
2021-09-04 12:38:56 +0100

    minor changes

--- glass/__init__.py
+++ glass/__init__.py
@@ -2,7 +2,7 @@ from glass.sessions import session
 from glass.requests import request
 from glass.response import Response
 from glass.app import GlassApp
-from ._helpers import current_app
-from glass.response import redirect, flash, get_session_messages
+from ._helpers import current_app,flash, get_session_messages
+from glass.response import redirect
 
 from glass.templating import render_template, render_string
