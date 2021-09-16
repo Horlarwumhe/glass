0c0b9760bbe843999e79ae334f7b0d2dd69ba082
Horlarwumhe <amachiever4real@gmail.com>
2021-09-16 13:23:06 +0100

    Add new test file.

---
+++ test/test_mount.py
@@ -0,0 +1,42 @@
+from glass import GlassApp
+from glass import current_app
+from glass import render_string
+from glass import request
+
+import pytest
+
+app = GlassApp()
+
+
+def test_mount_error():
+    with pytest.raises(RuntimeError):
+        render_string("Hello {{name}}", name='glass')
+
+    with pytest.raises(RuntimeError):
+        current_app.config.get("KEY")
+
+    with pytest.raises(RuntimeError):
+        host = request.host
+
+
+def test_mount_success():
+    with app.mount():
+        assert bool(current_app) is True
+        assert app.config is current_app.config
+
+    assert bool(current_app) is False
+    with app.mount():
+        result = "Hello glass"
+        assert render_string("Hello {{name}}", name='glass') == result
+
+    with app.mount():
+        # this should raise an error.
+        # request object is not available.
+        # though the app is mouted, but environ argument is not provided.
+        with pytest.raises(RuntimeError):
+            host = request.host
+
+    environ = {}
+    environ['HTTP_USER_AGENT'] = 'curl'
+    with app.mount(environ):
+        assert request.user_agent == 'curl'
--- test/test_routing.py
+++ test/test_routing.py
@@ -6,15 +6,19 @@ from glass.routing import Rule, Router
 from glass.exception import HTTP404
 
 environ = {}
+
+
 def test_route():
     rule1 = Rule('/u/<user>/profile/')
     rule2 = Rule('/login/user/')
+    rule3 = Rule('/u/<int:user>/add/')
     router = Router()
     router.add(rule1)
     router.add(rule2)
+    router.add(rule3)
     environ['PATH_INFO'] = '/u/horlar/profile/'
     rule, kwargs = router.match(environ)
-    assert (rule is rule1)
+    assert rule is rule1
     assert kwargs == {'user': 'horlar'}
     environ['PATH_INFO'] = '/login/user/'
     rule, kwargs = router.match(environ)
@@ -23,5 +27,7 @@ def test_route():
     with pytest.raises(HTTP404):
         environ['PATH_INFO'] = '/user/login/'
         router.match(environ)
-
-
+    environ['PATH_INFO'] = '/u/90/add'
+    rule, kwargs = router.match(environ)
+    assert rule is rule3
+    assert kwargs == {'user': 90}
