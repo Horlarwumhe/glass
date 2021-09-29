60bc0eebcf53f51e577835bcc2876e3820878ae8
Horlarwumhe <amachiever4real@gmail.com>
2021-09-29 20:43:34 +0100

    add missing commit file.

---
+++ glass/template/utils.py
@@ -0,0 +1,31 @@
+import re
+
+smart_split_re = re.compile(r"""
+    ((?:
+        [^\s'"]*
+        (?:
+            (?:"(?:[^"\\]|\\.)*" | '(?:[^'\\]|\\.)*')
+            [^\s'"]*
+        )+
+    ) | \S+)
+""", re.VERBOSE)
+
+
+def smart_split(text):
+    # https://github.com/django/django/blob/main/LICENCE
+    """
+    Generator that splits a string by spaces, leaving quoted phrases together.
+    Supports both single and double quotes, and supports escaping quotes with
+    backslashes. In the output, strings will keep their initial and trailing
+    quote marks and escaped quotes will remain escaped.
+    >>> list(smart_split(r'This is "a person\'s" test.'))
+    ['This', 'is', '"a person\\\'s"', 'test.']
+    >>> list(smart_split(r"Another 'person\'s' test."))
+    ['Another', "'person\\'s'", 'test.']
+    >>> list(smart_split(r'A "\"funky\" style" test.'))
+    ['A', '"\\"funky\\" style"', 'test.']
+    """
+    split_content = []
+    for bit in smart_split_re.finditer(str(text)):
+        split_content.append(bit.group(0))
+    return split_content
